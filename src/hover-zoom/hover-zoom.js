75d1f5b2365f0c4beb755f10e13facff97607a57
Cmdv <vincent@cmdv.org>
2015-06-30 15:39:52 +0100

    image hover almost fixed, slight issue on screen rezise
    Signed-off-by: Cmdv <vincent@cmdv.org>

--- src/hover-zoom/hover-zoom.js
+++ src/hover-zoom/hover-zoom.js
@@ -6,38 +6,99 @@
 import Rx from 'rx';
 import $ from 'jquery';
 
-function hoverZoom (target) {
+var dw, dh, rw, rh, lx, ly;
 
-  var $target = $(target),
-      $flyout = $('<div class="easyzoom-flyout" />'),
-      $notice = $('<div class="easyzoom-notice" />');
+var $target = $('.hoverzoom'),
+  w1 = $target.width(),
+  h1 = $target.height(),
+  $flyout = $('<div class="hoverzoom-flyout" />'),
+  $link = $target.find('a'),
+  $image = $target.find('img'),
+  parentDiv = $('.thumbnail'),
+  link = $link.attr('href'),
+  zoomed = {},
+
+// setting up Observable
+  mouseEnter = Rx.Observable.fromEvent($target, 'mouseenter'),
+  mouseLeave = Rx.Observable.fromEvent($target, 'mouseleave'),
+  targetClick = Rx.Observable.fromEvent($target, 'click');
 
 
-  var mouseHover = Rx.Observable.fromEvent($target, 'mousemove');
-  var mouseEnter = Rx.Observable.fromEvent($target, 'mouseenter');
-  var mouseLeave = Rx.Observable.fromEvent($target, 'mouseleave');
+(function getStarted() {
+
+  var zoom = document.createElement("img");
+
+  zoom.style.position = 'absolute';
+  zoom.src = link;
+
+  $target.append($flyout);
+  $flyout.append(zoom);
+
+  zoomed = $('.hoverzoom-flyout').find('img');
+
+  $flyout.css({width: w1, height: h1});
+
+})();
+
+// prevent default clicks
+var preventDefault = targetClick.subscribe((e) => {
+  e.preventDefault();
+});
+
+// make sure $flyout is on DOM
+var _onEnter = mouseEnter.filter(() => $($flyout).length == 1);
 
 
-  var _onEnter = mouseEnter.map((e) => {
-      $target.append($flyout)
+var _entered = _onEnter.map(() => {
+
+  var w2, h2, padding,
+
+  w1 = $target.width(),
+  h1 = $target.height(),
+
+  w2 = $flyout.width();
+  h2 = $flyout.height();
+
+  dw = zoomed.width() - w2;
+  dh = zoomed.height() - h2;
+
+  rw = dw / w1;
+  rh = dh / h1;
+
+  padding = parentDiv.outerWidth();
+
+  $flyout.css({opacity: 1, left: padding, top: -20, width: w1, height: h1});
+
+}).filter(()=> zoomed.width() != 0);
+
+
+
+
+var enterSubscribe = _entered.subscribe(() => {
+
+  var mouseMove = Rx.Observable.fromEvent($target, 'mousemove').map((e) => {
+    lx = e.pageX || lx;
+    ly = e.pageY || ly;
+
+    var offset = $target.offset(),
+      pt = ly - offset.top,
+      pl = lx - offset.left,
+      xt = Math.ceil(pt * rh),
+      xl = Math.ceil(pl * rw),
+      top = xt * -1,
+      left = xl * -1;
+
+    return {top, left}
+
   });
 
-  var _onHover = mouseHover.map((hover) => {
-
-    return {left: hover.clientX, top: hover.clientY}
-  }).debounce(10);
-
-  var imageSubscribe = convert.subscribe((e) => {
-
-    var imageTop = (e.top * -yRatio) + (originalOffSets.top * yRatio),
-      imageLeft = (e.left * -xRatio) + (originalOffSets.left * xRatio);
-
-    $zoomImage.css({left: imageLeft, top: imageTop});
-    $zoom.css({opacity: 1});
+  var moveSubscribe = mouseMove.subscribe((a) => {
+    zoomed.css({top: a.top, left: a.left})
   });
 
-  imageLeave.subscribe(() => {
-    $zoom.css({opacity: 0});
-  });
+});
 
-}
+// hide flyout
+mouseLeave.subscribe(() => {
+  $flyout.css({opacity: 0});
+});
